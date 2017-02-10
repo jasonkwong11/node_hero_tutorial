21c4cea3186e52481aa5844e37c9577febad3a9d
Jason <jasonkwong11@gmail.com>
2017-02-10 05:51:34 -0500

    added handlebars notes

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 require('./app/index')
 
 const _ = require('lodash')
@@ -10,3 +11,35 @@ function isBiggerThanTwo (num) {
   return num > 2
 } 
 console.log(numbers.filter(isBiggerThanTwo));
+
+*/
+
+const path = require('path')
+const express = require('express')
+const exphbs = require('express-handlebars')
+
+//initialize handlebars engine and sets
+//layouts directory to views/layouts
+app.engine('.hbs', exphbs({
+  defaultLayout: 'main',
+  extname: '.hbs',
+  layoutsDir: path.join(_dirname, 'views/layouts')
+}))
+
+app.set('views engine', '.hbs')
+app.set('views', path.join(_dirname, 'views'))
+
+// add a route to express app
+  //render takes 2 params: 
+  // first is name of the view (home)
+  //second is the data you want to render
+app.get('/', (request, response) => {
+  response.render('home', {
+    name: 'Jason'
+  })
+})
+
+
+
+
+
